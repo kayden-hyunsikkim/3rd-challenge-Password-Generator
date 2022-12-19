3345cc60498a4250dbaaf35cd775d8e52fa1a4a6
kayden <117049896+kaydenhyunsikkim@users.noreply.github.com>
2022-12-19 17:58:33 +1030

    edit js for generatePassword function

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,56 @@
-// Assignment Code
-var usertype = [] ;
-var charlength = 1;
-var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',] ;
-var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','g','r','s','t','u','v','w','x','y','z',] ;
-var specialCase = ['~','!','@','#','$','%','^','&','*','(',')','_','-','=','+','/','|','<','>','.',','] ;
-var numberCase = ['0','1','2','3','4','5','6','7','8','9',] ;
+var chalength = " ";
+var usertype = [];
+var upperLetter =  ['A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J','K','L','M','N', 'O', 'P','Q','R','S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerLetter =  ['a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j','k','l','m','n', 'o', 'p','q','r','s', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var specialLetter = ['~','!','@','#','$','%','^','&','*','(',')','_','-','+','=','{','}','[',']','|',':',';','"','/','.',',','<','>'];
+var numBER = ['0','1','2','3','4','5','6','7','8','9'];
 
-var generateBtn = document.querySelector("#generate"); 
+var generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", WRIGHTPASSWORD);
 
-// Write password to the #password input
-function writePassword() {
-  var theValueofprompt = CallPrompt();
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+
+
+function WRIGHTPASSWORD() {
+    var runprompt  = Callprompt();
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
  
   
   
-  passwordText.value = password;
-
+    passwordText.value = password;
 
 }
 
-function CallPrompt() {
-charlength = parseInt(prompt("type charcters between 8 ~ 128 digits :)"));
-      if (isNaN(charlength) || charlength < 8 || charlength >128 ) {
-              alert('enter again.please');
-              return false;
-      }
-      if (confirm('Do you want to put upper character in your generated password?')) {
-              usertype = usertype.concat(upperCase);
-      }
-      if (confirm('Do you want to put lower character in your generated password?')) {
-              usertype = usertype.concat(lowerCase);
-      }
-      if (confirm('Do you want to put special character in your generated password?') ) {
-               usertype = usertype.concat(specialCase);
-      }
-      if (confirm('Do you want to put Number in your generated password?') ) {
-               usertype = usertype.concat(numberCase);
-      }
-      return true;
+
+
+
+function Callprompt() { 
+     chalength = parseInt( prompt("type letters you want for generated password between 8 ~ 128 letters,upper,lower,special, numbner"));
+   
+     if(isNaN(chalength) || chalength <8 || chalength >128) {
+        alert('try again');
+        return false;
+     }
+     if (confirm("Do you want to put uppercase in your generated password?")){
+        usertype = usertype.concat(upperLetter);
+     }
+     if (confirm("Do you want to put lowercase in your generated password?")){
+        usertype = usertype.concat(lowerLetter);
+     }
+     if (confirm("Do you want to put specialcase in your generated password?")){
+        usertype = usertype.concat(specialLetter);     }
+     if (confirm("Do you want to put number in your generated password?")){
+        usertype = usertype.concat(numBER);     }
+        return true;
+
 }
 
 function generatePassword() {
-      var password='';
-      for (i=0; i < usertype.length; i++) {
+    var password ='';
+    for(i=0; i < chalength; i++) {
         randomnumber = Math.floor(Math.random()*usertype.length);
         password = password + usertype[randomnumber];
-      }     
-      return password;
-
-}
-
-
-
+    }
+    return password;
+    }
